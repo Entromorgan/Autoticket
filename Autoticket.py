7b4c99adf2dad0e1143759efffae7bf930bfdb5e
Msute <956097926@qq.com>
2019-10-15 17:18:52 +0800

    修正票数定位问题2

--- Autoticket.py
+++ Autoticket.py
@@ -192,8 +192,8 @@ class Concert(object):
                 try:
                     for i in range(self.ticket_num - 1):  
                         addbtn = WebDriverWait(self.driver, self.total_wait_time, self.refresh_wait_time).until(
-                            EC.presence_of_element_located(
-                                (By.XPATH, "//div[@class='cafe-c-input-number']/a[2]")))
+                            EC.presence_of_element_located((By.XPATH, "//div[@class='cafe-c-input-number']/a[2]")))
+                        # print(EC)
                         addbtn.click()
                 except:
                     raise Exception("***错误：票数增加失败***")
